a50fbd83a3030d654ce8e87b5319d8fbec6bc723
Shankar R N S <shankarr-0701@shankarr-0701.dlink>
2019-06-04 15:40:12 +0530

    testing

--- weather.js
+++ weather.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const app = express()
-
+//est
 const apiKey = ' ';
 
 app.use(express.static('public'));
